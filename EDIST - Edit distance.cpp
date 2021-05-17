16100884cbe911dec74706695c319e6d1b2943e1
JayantGoel001 <jgoel92@gmail.com>
2021-05-17 12:43:07 +0530

    Initial Commit

--- EDIST - Edit distance.cpp
+++ EDIST - Edit distance.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
-#include <map>
 #define int long long int
 using namespace std;
-int editDistance(const string& str1,const string& str2,int n,int m,map<pair<int,int>,int> &mp){
-    if (mp.count(make_pair(n,m))!=0){
-        return mp[make_pair(n,m)];
+int editDistance(const string& str1,const string& str2,int n,int m,int **dp){
+    if (dp[n][m]!=-1){
+        return dp[n][m];
     }
     if (n==0 && m==0){
-        mp[make_pair(n,m)] = 0;
-        return mp[make_pair(n,m)];
+        dp[n][m] = 0;
+        return dp[n][m];
     }
     if (n==0){
-        mp[make_pair(n,m)] = m;
-        return mp[make_pair(n,m)];
+        dp[n][m] = m;
+        return dp[n][m];
     }
     if (m==0){
-        mp[make_pair(n,m)] = n;
-        return mp[make_pair(n,m)];
+        dp[n][m] = n;
+        return dp[n][m];
     }
     if (str1[n-1] == str2[m-1]){
-        mp[make_pair(n,m)] = editDistance(str1,str2,n-1,m-1,mp);
-        return mp[make_pair(n,m)];
+        dp[n][m] = editDistance(str1,str2,n-1,m-1,dp);
+        return dp[n][m];
     }
-    mp[make_pair(n,m)] = 1 + min(editDistance(str1,str2,n-1,m,mp), min(editDistance(str1,str2,n,m-1,mp), editDistance(str1,str2,n-1,m-1,mp)));
-    return mp[make_pair(n,m)];
+    dp[n][m] = 1 + min(editDistance(str1,str2,n-1,m,dp), min(editDistance(str1,str2,n,m-1,dp), editDistance(str1,str2,n-1,m-1,dp)));
+    return dp[n][m];
 }
 int32_t main(){
     ios_base::sync_with_stdio(false);
@@ -37,7 +36,17 @@ int32_t main(){
         cin>>str1;
         cin>>str2;
         cin.clear();
-        map<pair<int,int>,int> mp;
-        cout<<editDistance(str1,str2,str1.length(),str2.length(),mp)<<"\n";
+        int n= str1.length();
+        int m =str2.length();
+        int **dp = new int*[n+1];
+        for (int i = 0; i <=n; ++i) {
+            dp[i] = new int[m+1];
+        }
+        for (int i = 0; i <=n; ++i) {
+            for (int j = 0; j <=m; ++j) {
+                dp[i][j] =-1;
+            }
+        }
+        cout<<editDistance(str1,str2,n,m,dp)<<"\n";
     }
 }
