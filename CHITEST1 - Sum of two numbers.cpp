c5de3208c41e7b7957a542fd6e27a3e168f5b76f
JayantGoel001 <jgoel92@gmail.com>
2021-05-29 02:40:14 +0530

    Initial Commit

---
+++ CHITEST1 - Sum of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--){
+        float a,b;
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+}
