a194c32a7a74725dcd64261bd120484522fc337a
JayantGoel001 <jgoel92@gmail.com>
2021-06-13 16:03:55 +0530

    Initial Commit

--- LGIC - LOGIC.cpp
+++ LGIC - LOGIC.cpp
@@ -1,10 +1,6 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
 #define int long long int
-#define INF 1e18
-using namespace __gnu_pbds;
 using namespace std;
-typedef tree<pair<int, int>, null_type, less<pair<int , int>>, rb_tree_tag, tree_order_statistics_node_update > pbdS;
 int32_t main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
