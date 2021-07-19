766fbb88e5878628483b8390a843a6087d5d4176
JayantGoel001 <jgoel92@gmail.com>
2021-07-19 15:02:25 +0530

    Initial Commit

--- PRIME1 - Prime Generator.cpp
+++ PRIME1 - Prime Generator.cpp
@@ -1,34 +1,65 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 #define int long long int
 using namespace std;
-int32_t main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    int TOT = 1000000000;
-    bool isPrime[TOT+1];
-    for (int i = 0; i <=TOT; ++i) {
-        isPrime[i] = true;
+void sieve(vector<int> &prime,int N){
+    int limit = (int)sqrt(N);
+    bool isPrime[limit+1];
+    for (int i = 0; i <=limit; ++i) {
+        isPrime[i] = i%2;
     }
+    isPrime[0] = false;
     isPrime[1] = false;
-    for (int i = 2; i*i <=TOT ; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= TOT; j += i) {
+    isPrime[2] = true;
+    for (int i = 3; i*i <=limit; i+=2) {
+        if(isPrime[i]) {
+            for (int j = i * i; j <= limit; j += 2 * i) {
                 isPrime[j] = false;
             }
         }
     }
+    for (int i = 2; i <=limit; ++i) {
+        if (isPrime[i]){
+            prime.push_back(i);
+        }
+    }
+}
+void segmentedSieve(int low,int high){
+    vector<int> prime;
+    sieve(prime,high);
+    int segmentedLimit = high-low;
+    bool isSegmentedPrime[segmentedLimit+1];
+    for (int i = 0; i <= segmentedLimit; ++i) {
+        isSegmentedPrime[i] = true;
+    }
+    for (int i = 0; i < prime.size(); ++i) {
+        int j = low;
+        while (j<=high){
+            if (j%prime[i]==0 && j!=prime[i]) {
+                isSegmentedPrime[j - low] = false;
+            }
+            if (j%prime[i]==0){
+                j+=prime[i];
+            } else{
+                j++;
+            }
+        }
+    }
+    for (int i = low; i <= high; ++i) {
+        if (isSegmentedPrime[i-low] && i!=1){
+            cout<<i<<"\n";
+        }
+    }
+}
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
     int t;
     cin>>t;
     while (t--){
         int m,n;
         cin>>m>>n;
-        for (int i = m; i <=n; ++i) {
-            if (isPrime[i]){
-                cout<<i<<"\n";
-            }
-        }
+        segmentedSieve(m,n);
         cout<<"\n";
     }
 }
