e9bdb20b43c5139a13f75130ad1cc07e0c7a69b0
JayantGoel001 <jgoel92@gmail.com>
2021-07-31 01:18:41 +0530

    Initial Commit

---
+++ BUSYMAN - I AM VERY BUSY.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define int long long int
+#define double double_t
+#define INF 1e18
+using namespace __gnu_pbds;
+using namespace std;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int GCD(int a, int b) {
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+const int mod = 1000000000 + 7;
+
+int power(int x, int y, int MOD = mod) {
+    if (y == 0) {
+        return 1;
+    }
+    if (y % 2 == 0) {
+        return power((x * x) % MOD, y / 2, MOD) % MOD;
+    } else {
+        return (x * power((x * x) % MOD, (y - 1) / 2, MOD) % MOD) % MOD;
+    }
+}
+
+int inverse(int n, int p) {
+    return power(n, p - 2, p);
+}
+bool cmp(pair<int,int> p1,pair<int,int> p2){
+    return p1.second<p2.second;
+}
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        pair<int,int> ar[n];
+        for (int i = 0; i < n; ++i) {
+            cin>>ar[i].first>>ar[i].second;
+        }
+        sort(ar,ar+n,cmp);
+        int count = 0;
+        int start = -1;
+        for (int i = 0; i < n; ++i) {
+            if(ar[i].first>=start){
+                start = ar[i].second;
+                count++;
+            }
+        }
+        cout<<count;
+        cout << "\n";
+    }
+}
